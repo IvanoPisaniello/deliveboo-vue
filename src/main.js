221360842deff853ef1af0cb850ccab033a6bccd
ElisaIanda <elisaianda01@gmail.com>
2023-11-03 11:48:52 +0100

    Fix name file

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import './styles/general.scss'
 
 import App from './App.vue'
-import { router } from './route'
+import { router } from './router'
 
 createApp(App).use(router).mount('#app')
--- src/router.js
+++ src/router.js
@@ -7,18 +7,18 @@ import { createRouter, createWebHistory } from "vue-router";
 
 // definiamo le rotte
 const routes = [
-    {
-        path: "/", // uri da scrivere nel browser
-        name: "home", // nome della rotta da usare per creare un link
-        component: HomePage // componente che ritorna l'html della pagina
-    },
-    // ... altre rotte
-    {
-        // Show di un singolo post
-        path: "/posts/:id",
-        name: "posts.show",
-        component: PostsShowPage
-    }
+    // {
+    //     path: "/", // uri da scrivere nel browser
+    //     name: "home", // nome della rotta da usare per creare un link
+    //     component: HomePage // componente che ritorna l'html della pagina
+    // },
+    // // ... altre rotte
+    // {
+    //     // Show di un singolo post
+    //     path: "/posts/:id",
+    //     name: "posts.show",
+    //     component: PostsShowPage
+    // }
 ]
 
 // creazione istanza di Router
