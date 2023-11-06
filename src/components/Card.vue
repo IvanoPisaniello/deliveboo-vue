b23ca11b645bf5119c6881de03537229c67d8f3e
MatteoArt <mattrim8@gmail.com>
2023-11-06 12:17:23 +0100

    fix grafic card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -16,8 +16,7 @@ export default {
         <div class="card-body">
             <h5 class="card-title"> {{ singleCard.name }} </h5>
             <h6 class="card-subtitle mb-2 text-body-secondary">  {{ singleCard.address }} </h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                content.</p>
+            <p class="card-text"></p>
             <a href="#" class="card-link">Card link</a>
             <a href="#" class="card-link">Another link</a>
         </div>
