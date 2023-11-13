d045093dcdcdef7704795879e2cb73b587900a6e
ElisaIanda <elisaianda01@gmail.com>
2023-11-13 18:41:29 +0100

    fix style

--- src/pages/PaymentSuccess.vue
+++ src/pages/PaymentSuccess.vue
@@ -1,4 +1,17 @@
 <script></script>
 <template>
-  <h1 class="text-success">Pagamento Riuscito</h1>
+  <main>
+    <div class="container">
+      <div class="text-center">
+        <h1 class="">Il pagamento è stato effettuato con successo</h1>
+        <div class="col">
+          <img
+            src="https://img.freepik.com/premium-vector/people-paying-successfully-with-phone-online-mobile-payment-banking-service_530733-2928.jpg"
+            alt="">
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
+
+<style lang="scss" scoped>@use "../src/styles/partials/variable" as *;</style>
--- src/pages/RestaurantShow.vue
+++ src/pages/RestaurantShow.vue
@@ -41,52 +41,51 @@ export default {
 </script>
 
 <template>
-    <div class="container" id="padding">
-        <h1> {{ restaurant.name }} </h1>
-        <span class="text-primary-emphasis"> {{ restaurant.address }} </span>
-        <div>
-            <span v-for="type in restaurant.types" class="text-danger-emphasis d-inline-block fw-semibold me-2">
-                {{ type.name }}
-            </span>
-        </div>
-        <h3 class="mt-3">I nostri piatti</h3>
-        <div class="row">
-            <div v-for="dish in restaurant.dishes"
-                :class="{ 'col-sm-6 col-lg-4 mb-5': dish.visible, 'd-none': !dish.visible }">
-                <div class="card" style="height: 500px;">
-                    <img :src="getImagePath(dish.image)" class="card-img-top" alt="img_dish"
-                        style="height: 190px;object-fit: cover;">
-                    <div class="card-body d-flex flex-column">
-                        <h5 class="card-title"> {{ dish.title }} </h5>
-                        <div>
-                            <span v-if="dish.discount != null && dish.discount > 0"
-                                class="badge rounded-pill text-bg-success"> -{{ dish.discount
-                                }}%
-                            </span>
-                        </div>
-                        <p class="card-text">
-                            {{ dish.description }}
-                        </p>
-                        <span class="fst-italic">Ingredienti: </span>
-                        <p class="card-text fst-italic">
-                            {{ dish.ingredients }}
-                        </p>
-                        <div class="mt-auto"> {{ dish.price }}€ </div>
-                        <div class="d-flex my-2">
-                            <button @click="incrementCount(dish)" class="btn bg-orange-text-white">Aggiungi al Carrello</button>
+    <main>
+        <div class="container" id="padding">
+            <h1> {{ restaurant.name }} </h1>
+            <span class="text-primary-emphasis"> {{ restaurant.address }} </span>
+            <div>
+                <span v-for="type in restaurant.types" class="text-danger-emphasis d-inline-block fw-semibold me-2">
+                    {{ type.name }}
+                </span>
+            </div>
+            <h3 class="mt-3">I nostri piatti</h3>
+            <div class="row">
+                <div v-for="dish in restaurant.dishes"
+                    :class="{ 'col-sm-6 col-lg-4 mb-5': dish.visible, 'd-none': !dish.visible }">
+                    <div class="card" style="height: 500px;">
+                        <img :src="getImagePath(dish.image)" class="card-img-top" alt="img_dish"
+                            style="height: 190px;object-fit: cover;">
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title"> {{ dish.title }} </h5>
+                            <div>
+                                <span v-if="dish.discount != null && dish.discount > 0"
+                                    class="badge rounded-pill text-bg-success"> -{{ dish.discount
+                                    }}%
+                                </span>
+                            </div>
+                            <p class="card-text">
+                                {{ dish.description }}
+                            </p>
+                            <span class="fst-italic">Ingredienti: </span>
+                            <p class="card-text fst-italic">
+                                {{ dish.ingredients }}
+                            </p>
+                            <div class="mt-auto"> {{ dish.price }}€ </div>
+                            <div class="d-flex my-2">
+                                <button @click="incrementCount(dish)" class="btn bg-orange-text-white">Aggiungi al
+                                    Carrello</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-    </div>
+        </div>
+    </main>
 </template>
 
 <style lang="scss" scoped>
 @use "../src/styles/partials/variable" as *;
-
-#padding {
-    padding-top: $padding-container;
-}
 </style>
