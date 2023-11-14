be9c35af3606f76da75a942aaa9db6f93b8996b3
IvanoPisaniello <ivanop92@gmail.com>
2023-11-14 09:00:09 +0100

    added image in card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,6 +6,11 @@ export default {
             type: Object,
             required: true
         }
+    },
+    methods: {
+        getImagePath(image) {
+            return `http://127.0.0.1:8000/storage/${image}`;
+        }
     }
 }
 </script>
@@ -13,15 +18,15 @@ export default {
 <template>
     <div class="col-sm-6 col-md-4 col-lg-2 py-4">
         <div class="card h-100 ">
-            <!-- <img src="..." class="card-img-top" alt="..."> -->
+            <img :src="getImagePath(singleCard.image)" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title"> {{ singleCard.name }} </h5>
                 <h6 class="card-subtitle mb-2 text-body-secondary"> {{ singleCard.address }} </h6>
                 <div class="card-text">
                     <div v-for="type in singleCard.types"> {{ type.name }} </div>
                 </div>
-                <router-link :to="{ name: 'restaurant.show', params: { slug: singleCard.slug } }" 
-                class="card-link">Dettagli ristorante</router-link>
+                <router-link :to="{ name: 'restaurant.show', params: { slug: singleCard.slug } }" class="card-link">Dettagli
+                    ristorante</router-link>
             </div>
         </div>
     </div>
